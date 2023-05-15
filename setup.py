548f4c5ee58450e8fa6c33eb9862243fa8e677f3
official-aryan <official.aryan0909@gmail.com>
2023-05-16 00:28:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    get_requirements=[]
+    with open(file_path) as file_obj:
+       requirements=file_obj.readline()
+       requirements= [req.replace("\n","")for req in requirements]
+       
+       return requirements
+
+setup(
+    
+    name="Diamond_price_prediction",
+    version="0.0.1",
+    author="Aryan",
+    author_email="aryanprasadannonymous@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
